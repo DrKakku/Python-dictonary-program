27edc648229a6cd928880247d62a43d41874b272
Yash Tripathi <53153448+DrKakku@users.noreply.github.com>
2020-04-01 16:45:51 +0530

    Beautify update
    added the support of printing the jason lists and added color😍😍😍
    
    made the code more spacious

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import json
+import sys
 
 
 data = json.load(open("data.json"))
 
 def meaning (word):
+
+
     try:
-        return data[word]
+
+        defe = data[word]
+
+
+        if type(defe) == list:
+
+            length = len(defe)
+
+            for i in range(length) :
+                if i %2 == 0 :
+                    print("\n \033[33m =",defe[i])
+                else:
+                    print("\n \033[35m =",defe[i])
+    
+        else:
+
+            print(defe)
+    
+    
     except KeyError:
-        print("the word you entered does not exist in this dictonary sorry ಥ_ಥ")
-        return("please try anain")        
+
+        print("\n\n\033[31m the word you entered does not exist in this dictonary sorry ಥ_ಥ")
+
+        return("\033[31m please try angain")        
+
+
 
 print("this is a dictonary program")
 
@@ -20,16 +45,17 @@ flag = True
 while flag != False:
 
     
-    print(meaning(word.lower()),"\n\n")
+    meaning(word.lower())
 
 
    
-    print("\n \n do you wish to find another word's defination \n(if no then type no \nand if yess just type the word and hit enter)")
+    print("\n \n \033[32m do you wish to find another word's defination \n(if no then type no \nand if yess just type the word and hit enter)")
 
 
-    word =  input(":-  ")
+    word =  input("\033[37m :-  ")
 
     if word == "no" or  word == "No":
+
         flag = False
 
 print("Thank you for using the program \n good bye")
